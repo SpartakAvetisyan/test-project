c8c881c14564fd2edcc43a826e821fc26c22ae9c
spartakavetisyan <spartakavetisyan1994@mail.ru>
2019-03-05 21:28:24 +0400

    first commit

---
+++ app/src/main/java/com/example/a4steps/teamath/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.a4steps.teamath;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_sign_up_step_three);
+    }
+}
