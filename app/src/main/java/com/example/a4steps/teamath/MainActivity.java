b4108db5c92ee6fc2f4228a73c258e6e9b0650d7
spartakavetisyan <spartakavetisyan1994@mail.ru>
2019-03-06 21:08:47 +0400

    tab tow

--- app/src/main/java/com/example/a4steps/teamath/MainActivity.java
+++ app/src/main/java/com/example/a4steps/teamath/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_sign_up_step_two);
+        setContentView(R.layout.tab_two);
     }
 }
