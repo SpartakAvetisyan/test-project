33642b96994dd37d07590f4ef8bafd8382cc1e09
step <spartakavetisyan@mail.ru>
2019-03-07 07:43:10 +0400

    profile

--- app/src/main/java/com/example/a4steps/teamath/MainActivity.java
+++ app/src/main/java/com/example/a4steps/teamath/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.tab_forth);
+        setContentView(R.layout.user_profile);
     }
 }
