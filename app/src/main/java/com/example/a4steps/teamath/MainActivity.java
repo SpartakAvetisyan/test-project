49093aebf270637b97853d8da9f220f39639dfb7
spartakavetisyan <spartakavetisyan1994@mail.ru>
2019-03-07 09:26:53 +0400

    chat input

--- app/src/main/java/com/example/a4steps/teamath/MainActivity.java
+++ app/src/main/java/com/example/a4steps/teamath/MainActivity.java
@@ -1,14 +1,14 @@
 package com.example.a4steps.teamath;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.user_profile);
+        setContentView(R.layout.activity_chat);
     }
 }
