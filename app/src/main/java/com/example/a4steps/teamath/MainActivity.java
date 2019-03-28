5998677f59bb368851651f5d0be5cc5ea4e6224f
spartakavetisyan <spartakavetisyan1994@mail.ru>
2019-03-28 18:11:37 +0400

    comment

--- app/src/main/java/com/example/a4steps/teamath/MainActivity.java
+++ app/src/main/java/com/example/a4steps/teamath/MainActivity.java
@@ -1,14 +1,36 @@
 package com.example.a4steps.teamath;
 
+import android.graphics.drawable.AnimatedVectorDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.vectordrawable.graphics.drawable.AnimatedVectorDrawableCompat;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.add_post);
+        setContentView(R.layout.test);
+
+
+    }
+
+
+    public void animate(View view) {
+        ImageView v = (ImageView) view;
+        Drawable d = v.getDrawable();
+        if (d instanceof AnimatedVectorDrawable) {
+            AnimatedVectorDrawable avd = (AnimatedVectorDrawable) d;
+            avd.start();
+        } else if (d instanceof AnimatedVectorDrawableCompat) {
+            AnimatedVectorDrawableCompat avd = (AnimatedVectorDrawableCompat) d;
+            avd.start();
+        }
     }
 }
+
